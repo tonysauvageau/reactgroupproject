cdb74e48a85e7902598bcd6a5357aafbc9445895
tonysauvageau <toneeees@gmail.com>
2017-02-16 18:34:15 -0700

    removed code for shawn

--- src/PhonebookForm.js
+++ src/PhonebookForm.js
@@ -2,26 +2,13 @@ import React from 'react';
 
 class PhonebookForm extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { text: '' }
-    }
 
-    addContact = (e) => {
-        e.preventDefault();
-        let { name, number, form } = this.refs;
-        this.props.addTodoItem( name.value, number.value)
-        form.reset();
-    }
     
 
     render(){
         return (
             <div>
-                <form ref="form" onSubmit={this.addContact}>
-                    <input ref="name" placeholder="Name" required={true} />
-                    <input ref="number" placeholder="Number" required={true} />
-                </form>
+                
             </div>
         );
     }
